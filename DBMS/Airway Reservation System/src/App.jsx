cf1b8ea9b87b9effb5d14990eb468703519e93de
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-11-05 20:31:35 +0530

    Created jsx pages for each component in src folder

--- DBMS/Airway Reservation System/src/App.jsx
+++ DBMS/Airway Reservation System/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import Navbar from './Components/Navbar/navbar'
+import Navbar from './Components/Navbar/navbar';
+import Home from './Components/Home/home';
+import Footer from './Components/Footer/footer';
+
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <Footer/>
     </div>
   )
 }
---
+++ DBMS/Airway Reservation System/src/Components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const footer = () => {
+    return(
+        <button>Footer</button>
+    )
+}
+
+export default footer;
